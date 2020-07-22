c4fadf0e869c00bf69bc115706c58344034bf4d4
Junior Trojilio <osmar.trojilio@gmail.com>
2020-07-22 15:21:43 -0300

    new folder

---
+++ dist/precache-manifest.23985e6bf1d5911d1aaa3a193a2d9658.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b9ef7c8264365e06cdcbaeb2fc8f994",
+    "url": "/index.html"
+  },
+  {
+    "revision": "302d389dfb97045f419d",
+    "url": "/static/js/2.27e7c71f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.27e7c71f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5821e9f5e221b0576ac8",
+    "url": "/static/js/main.08c42710.chunk.js"
+  },
+  {
+    "revision": "23a8542c495de0a29ff2",
+    "url": "/static/js/runtime-main.f51e40a7.js"
+  },
+  {
+    "revision": "0b819d63338019a0f1cc7263e9c166ae",
+    "url": "/static/media/github-background.0b819d63.svg"
+  },
+  {
+    "revision": "04f6798f67fd4e48d053d05d18d9bd83",
+    "url": "/static/media/logo.04f6798f.svg"
+  }
+]);
